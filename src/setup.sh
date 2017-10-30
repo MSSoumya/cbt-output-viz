2d4e9ad99fd29a9884310e972466457887b5c3ee
soumya <ms.soumya12@gmail.com>
2017-10-30 22:15:29 +0530

    add script to install dependencies and setup virtualenv

---
+++ src/setup.sh
@@ -0,0 +1,27 @@
+
+#! bash script for setting up enviornment for flask app
+
+
+sudo apt-get install python-virtualenv
+
+virtualenv flask
+
+#proxy="--proxy http://proxy.iiit.ac.in:8080 "
+
+proxy=""
+
+flask/bin/pip $proxy install flask
+
+flask/bin/pip $proxy install flask-login
+
+flask/bin/pip $proxy install requests
+
+flask/bin/pip $proxy install requests
+
+flask/bin/pip $proxy install bs4
+
+flask/bin/pip $proxy install python-dateutil
+
+flask/bin/pip $proxy install datetime
+
+flask/bin/pip $proxy install -U flask-cors
